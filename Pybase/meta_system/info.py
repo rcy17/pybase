4135e548c07846a0469f59fa1c5ea3f7227874ab
DongHaowen <dhw17.tech.tsinghua@gmail.com>
2020-12-15 00:03:55 +0800

    Fix some bugs

--- Pybase/meta_system/info.py
+++ Pybase/meta_system/info.py
@@ -1,3 +1,4 @@
+from Pybase.exceptions.run_sql import DataBaseError
 from Pybase.exceptions.meta import TableExistenceError, ColumnExistenceError
 
 class ColumnInfo:
@@ -25,20 +26,22 @@ class TableInfo:
     def __init__(self, name, colList, orderList = None) -> None:
         self._name = name
         self._colMap = {col._name: col for col in colList}
+        self.primary = []
+        self.foreign = {}
         if orderList is None:
             self._colindex = {col:i for i, col in enumerate(colList)}
         else:
             self._colindex = {col:i for i, col in zip(colList, orderList)}
     
     def insert_column(self, column: ColumnInfo, colindex: int):
-        if self._colMap.get(column._name) is None:
+        if column._name in self._colMap:
             self._colMap[column._name] = column
             self._colindex[column._name] = colindex
         else:
             raise ColumnExistenceError(f"Column {column._name} should not exists.")
     
     def remove_column(self, colname):
-        if self._colMap.get(colname) is None:
+        if colname in self._colMap:
             raise ColumnExistenceError(f"Column {colname} should exists.")
         else:
             self._colindex.pop(colname)
@@ -46,6 +49,12 @@ class TableInfo:
     
     def get_size(self) -> int:
         return sum([col.get_size() for col in self._colMap.values()])
+    
+    def set_primary(self, cols):
+        self.primary = cols
+    
+    def set_foriegn(self, col, foreign_col):
+        self.foreign[col] = foreign_col
 
 
 class DbInfo:
@@ -54,26 +63,27 @@ class DbInfo:
         self._tbMap = {tb._name: tb for tb in tbList}
     
     def insert_table(self, table: TableInfo):
-        if self._tbMap.get(table._name) is None:
+        if table._name in self._tbMap:
             self._tbMap[table._name] = table
         else:
             raise TableExistenceError(f"Table {table._name} should not exists.")
     
     def insert_column(self, tbname, column: ColumnInfo, colindex: int):
-        if self._tbMap.get(tbname) is None:
+        if tbname in self._tbMap:
             raise TableExistenceError(f"Table {tbname} should exists.")
         else:
-            self._tbMap[tbname].insert_column(column, colindex)
+            table : TableInfo = self._tbMap[tbname]
+            table.insert_column(column, colindex)
     
     def remove_table(self, tbname):
-        if self._tbMap.get(tbname) is None:
+        if tbname in self._tbMap:
             raise TableExistenceError(f"Table {tbname} should exists.")
         else:
             self._tbMap.pop(tbname)
 
     def remove_column(self, tbname, colname):
-        if self._tbMap.get(tbname) is None:
+        if tbname in self._tbMap:
             raise TableExistenceError(f"Table {tbname} should exists.")
         else:
-            table = self._tbMap.get(tbname)
+            table: TableInfo = self._tbMap[tbname]
             table.remove_column(colname)
