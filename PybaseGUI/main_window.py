ceb940228af5cde93e2dd82da0e8b4cc39451c3d
rcy <rcycyzh@163.com>
2020-12-31 11:18:00 +0800

    Finish child thread

--- PybaseGUI/main_window.py
+++ PybaseGUI/main_window.py
@@ -3,23 +3,25 @@ Here defines MainWindow for GUI
 
 Date: 2020/12/30
 """
+from typing import Union, Tuple
 from pathlib import Path
 from multiprocessing.connection import Connection
 from datetime import timedelta, datetime
 from enum import Enum
+from queue import Queue
 
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5.QtGui import QStandardItemModel, QKeyEvent
-from PyQt5.QtCore import QModelIndex, QTimer, Qt
+from PyQt5.QtCore import QModelIndex, QTimer, Qt, QMutex, QWaitCondition, pyqtSlot
 
 from .ui.main_window import Ui_MainWindow
-from .worker import ReadOnlyItem
-from Pybase.manage_system.result import QueryResult
+from .worker import ReadOnlyItem, Worker
 
 
 class Status(Enum):
     Waiting = '空闲'
     Running = '运行中'
+    Building = '加载中'
     Rendering = '渲染中'
 
 
@@ -31,26 +33,46 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.connection = connection
         self.base_path = base
         self.tree_file.setModel(QStandardItemModel())
-        self.table_result.setModel(QStandardItemModel())
         self.load_tree_file()
 
         self.timer = QTimer(self)
-        self.timer.setInterval(10)
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.check_result)
+
+        self.mutex = QMutex()
+        self.wait_condition = QWaitCondition()
+        self.queue = Queue(1)
+        self.worker = Worker(connection, self.mutex, self.wait_condition, self.queue)
+        self.worker.executed.connect(self.worker_execute)
+        self.worker.finished.connect(self.worker_finish)
+        self.worker.start()
 
         self.last_start = None
         self.status = None
         self.cost = None
-        self.models = []
+        self.results = []
+        self.tasks = []
 
         self.set_status(Status.Waiting)
         self.set_result_report()
+
+    @pyqtSlot()
+    def worker_execute(self):
+        self.set_status(Status.Building)
+
+    @pyqtSlot(tuple)
+    def worker_finish(self, args: Tuple[Union[QStandardItemModel, None, str], timedelta]):
+        result, cost = args
+        self.set_status(Status.Rendering)
+        self.show_result(result, cost)
+        self.timer.stop()
+        self.set_status(Status.Waiting)
 
     def set_status(self, status: Status, cost=None):
         self.status = status
         msg = '状态：' + status.value
         if cost:
-            msg += '，已进行%.2fs' % cost.total_seconds()
+            msg += '，已进行%.1fs' % cost.total_seconds()
         self.statusbar.showMessage(msg)
 
     def set_result_report(self, size: int = 0, cost: timedelta = timedelta(0)):
@@ -72,42 +94,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 table.appendRow(ReadOnlyItem(file.stem))
                 schema.appendRow(ReadOnlyItem(file.stem))
 
-    def show_result(self, result: QueryResult, cost: timedelta):
+    def show_result(self, result: Union[QStandardItemModel, None, str], cost: timedelta):
         if result is None:
             self.set_result_report(0, cost)
             return
-        if result.message:
+        if isinstance(result, str):
             self.set_result_report(0, cost)
-            QMessageBox.critical(self, 'error', result.message)
+            QMessageBox.critical(self, 'error', result)
             return
-        model: QStandardItemModel = self.table_result.model()
-        model.clear()
-        model.setHorizontalHeaderLabels(result.headers)
-        for row in result.data:
-            model.appendRow(ReadOnlyItem(str(item)) for item in row)
-        self.set_result_report(len(result.data), cost)
+        self.results.append((result, cost))
+        self.table_result.setModel(result)
+        self.table_result.resizeColumnsToContents()
+        self.set_result_report(result.rowCount(), cost)
 
     def run_sql(self, sql):
         if self.status != Status.Waiting:
             QMessageBox.critical(self, 'error', '请等待当前SQL运行、渲染完成后再执行新的SQL')
             return
-        self.connection.send(sql)
+        self.mutex.lock()
+        self.queue.put(sql)
+        self.wait_condition.wakeAll()
+        self.mutex.unlock()
         self.timer.start()
         self.last_start = datetime.now()
         self.set_status(Status.Running)
-        self.check_result()
 
     def check_result(self):
         cost = datetime.now() - self.last_start
-        if self.connection.poll():
-            self.timer.stop()
-            result = self.connection.recv()
-            self.set_status(Status.Rendering)
-            self.statusbar.repaint()  # Force to show rendering status
-            self.show_result(result, cost)
-            self.set_status(Status.Waiting)
-        else:
-            self.set_status(Status.Running, cost)
+        self.set_status(self.status, cost)
 
     def on_tree_file_doubleClicked(self, index: QModelIndex):
         if index.parent().data() == 'table':
--- PybaseGUI/worker.py
+++ PybaseGUI/worker.py
@@ -3,10 +3,15 @@ Worker is a thread to interactive with SQL processing
 
 Date: 2020/12/31
 """
-from datetime import timedelta
+from typing import Tuple, Union
+from datetime import datetime, timedelta
+from multiprocessing.connection import Connection
+from queue import Queue
 
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QMutex, QWaitCondition
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
+
+from Pybase.manage_system.result import QueryResult
 
 
 class ReadOnlyItem(QStandardItem):
@@ -16,8 +21,46 @@ class ReadOnlyItem(QStandardItem):
 
 
 class Worker(QThread):
-    executed = pyqtSignal(timedelta)
-    finished = pyqtSignal(QStandardItemModel)
+    executed = pyqtSignal()
+    finished = pyqtSignal(tuple)
+
+    def __init__(self, connection: Connection, mutex: QMutex, wait_condition: QWaitCondition, queue: Queue):
+        super(Worker, self).__init__()
+        self.connection = connection
+        self.mutex = mutex
+        self.wait_condition = wait_condition
+        self.queue = queue
+
+    @staticmethod
+    def make_model(result: QueryResult) -> Union[None, str, QStandardItemModel]:
+        if result is None:
+            return
+        if result.message:
+            return result.message
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(result.headers)
+        for row in result.data:
+            model.appendRow(ReadOnlyItem(str(item)) for item in row)
+        return model
+
+    def work(self, sql) -> Tuple[QueryResult, timedelta]:
+        self.connection.send(sql)
+        start = datetime.now()
+        result: QueryResult = self.connection.recv()
+        stop = datetime.now()
+        return result, stop - start
+
+    def wait_task(self):
+        self.mutex.lock()
+        self.wait_condition.wait(self.mutex)
+        sql = self.queue.get()
+        self.mutex.unlock()
+        return sql
 
     def run(self) -> None:
-        pass
+        while True:
+            task = self.wait_task()
+            result, cost = self.work(task)
+            self.executed.emit()
+            model = self.make_model(result)
+            self.finished.emit((model, cost))
